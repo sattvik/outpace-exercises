4cae94e162f03d1b00cb95213510c29bef031697
Daniel Solano Gómez <daniel@solanogomez.org>
2014-12-21 19:52:07 -0600

    Add lines->digits function

--- src/outpace_exercise/ocr.clj
+++ src/outpace_exercise/ocr.clj
@@ -14,3 +14,18 @@
     " _   |  |" 7
     " _ |_||_|" 8
     " _ |_| _|" 9))
+
+(defn line->digits
+  "Converts a line of 'pixelated' OCR digits into a vector of digits."
+  [lines]
+  (->> (take 3 lines)
+       ; partition each line into groups of three pixels (one character)
+       (map (fn [line] (partition 3 line)))
+       ; transpose the lines/characters
+       (apply map list)
+       ; concatenate each row of pixels into one 9-pixel array
+       (map (fn [pixel-line] (apply concat pixel-line)))
+       ; convert the pixels array into a string
+       (map (fn [pixels] (apply str pixels)))
+       ; convert the string into a digit
+       (map pixels->digit)))
